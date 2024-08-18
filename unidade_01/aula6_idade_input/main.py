3a8c1b57eafa5370b21ed32dd234fed17095266d
Marcelo saad <marcelo_saad@hotmail.com>
2024-08-18 14:42:22 -0400

    lint correct

--- unidade_01/aula6_idade_input/main.py
+++ unidade_01/aula6_idade_input/main.py
@@ -1,7 +1,8 @@
-ano_nascimento =input('Em que ano você nasceu ')
+ano_nascimento = input("Em que ano você nasceu ")
 print(type(ano_nascimento))
 
 
-idade = 2022 - int(ano_nascimento)
+idade = 2024 - int(ano_nascimento)
 
 print(idade)
+
